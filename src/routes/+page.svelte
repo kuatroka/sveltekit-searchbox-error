6fb047cc1c4819c87dc12fbc89b798b52d741cf3
kuatroka <aaarmani_2000@yahoo.com>
2023-12-22 22:05:51 +0000

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+import search_reset from '$lib/images/search_reset.gif'
+
+</script>
+
+<div class="flex flex-col items-center justify-center min-h-screen">
+    <h1 class="text-center text-4xl mb-4">Go to 'Search-box-problem' menu on the top right</h1>
+    <!-- <img src={search_reset} alt="Search Reset GIF" class="w-auto h-auto" /> -->
+  </div>
 
